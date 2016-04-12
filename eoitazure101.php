24a32d1cfad3efcbf18d6e7ee658689d96160d40
AJ Eaves <andrew@eavesofit.com>
2016-04-12 16:52:47 -0400

    Update eoitazure101.php
    Add a new paragraph

--- eoitazure101.php
+++ eoitazure101.php
@@ -3,6 +3,7 @@
   <title>EoitAzure101 Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World, from Azure 101</p>'; ?> 
+ <?php echo '<p>Hello World, from Azure 101</p>'; ?>
+ <p>This is v1.0</p>
  </body>
 </html>
